29b8599f2c428abfd07c259abcc26fdd870545b0
Artiola Shkurtaj <artiolaskourtis@gmail.com>
2025-05-06 18:35:16 -0400

    Language-based matching

--- server.js
+++ server.js
@@ -6,26 +6,50 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the public folder
 app.use(express.static("public"));
+
+let waitingUsers = {}; // key = language, value = array of sockets
 
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.on("findPartner", (prefs) => {
-    console.log("Preferences:", prefs);
-    socket.emit("partnerFound");
+  socket.on("findPartner", ({ language, topic, feeling }) => {
+    socket.language = language;
+
+    if (!waitingUsers[language]) {
+      waitingUsers[language] = [];
+    }
+
+    const partner = waitingUsers[language].find(s => s !== socket && !s.partner);
+    
+    if (partner) {
+      // Pair them
+      socket.partner = partner;
+      partner.partner = socket;
+
+      socket.emit("partnerFound");
+      partner.emit("partnerFound");
+    } else {
+      waitingUsers[language].push(socket);
+    }
   });
 
   socket.on("sendMessage", (msg) => {
-    socket.broadcast.emit("receiveMessage", msg);
+    if (socket.partner) {
+      socket.partner.emit("receiveMessage", msg);
+    }
   });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("partnerDisconnected");
+    if (socket.partner) {
+      socket.partner.emit("partnerDisconnected");
+      socket.partner.partner = null;
+    }
+    if (waitingUsers[socket.language]) {
+      waitingUsers[socket.language] = waitingUsers[socket.language].filter(s => s !== socket);
+    }
   });
 });
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
